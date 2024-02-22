aba2314d7199a719471a0d1ef4055c0fc308b075
jeffch19 <17jcummings@gmail.com>
2024-02-22 13:34:32 -0500

    added to server.js and installed appollo

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas'); 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT ||  3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,6 +15,16 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+// Create an instance of Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req }), // Pass the request object to the context
+});
+
+// Apply Apollo Server as middleware to the Express app
+server.applyMiddleware({ app });
 
 app.use(routes);
 
