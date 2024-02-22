56f1b02588980a2143e30ba7a35ddc3f446a61b0
jeffch19 <17jcummings@gmail.com>
2024-02-22 13:25:53 -0500

    added to auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,27 +7,30 @@ const expiration = '2h';
 module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+    // Check if the request is a GraphQL request
+    if (req.body && req.body.query && req.body.query.includes('mutation')) {
+      // Extract token from the request body
+      let token = req.body.token || req.headers.authorization;
+  
+      // ["Bearer", "<tokenvalue>"]
+      if (req.headers.authorization) {
+        token = token.split(' ').pop().trim();
+      }
+  
+      if (!token) {
+        return res.status(400).json({ message: 'You have no token!' });
+      }
+  
+      // verify token and get user data out of it
+      try {
+        const { data } = jwt.verify(token, secret, { maxAge: expiration });
+        req.user = data;
+      } catch {
+        console.log('Invalid token');
+        return res.status(400).json({ message: 'invalid token!' });
+      }
     }
-
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
-
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
-
+  
     // send to next endpoint
     next();
   },
