9974e6a629896ddd2c999eabe2c9fa9085ffcbde
jeffch19 <17jcummings@gmail.com>
2024-02-22 15:03:14 -0500

    debugging

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../utils/mutations'; // Adjust the import path as necessary
+import { LOGIN_USER } from '../graphql/mutations'; // Adjust the import path as necessary
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
